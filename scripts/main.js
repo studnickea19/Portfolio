585039d9b76434201b9d96f6f59e473db10f7f2b
studnickea19 <36928178+studnickea19@users.noreply.github.com>
2018-06-22 11:52:07 -0500

    Cleaning up classes, Project section is displaying properly

--- scripts/main.js
+++ scripts/main.js
@@ -2,15 +2,15 @@ $(document).ready(function(){
 
 	$('project-details').hide();
 
-	$('div.language-slider').mouseenter(function(){
+	$('div.language-slider, img.details-button').mouseenter(function(){
 		$(this).css("background-color", "#CACFD2");
 	}).mouseleave(function(){
 		$(this).css("background-color", "#D7DBDD");
 	});
 
-	// $('div.details-button').click(function(){
-	// 	$(this).closest('div.project-details')show();
-	// });
+	$('div.details-button').click(function(){
+		$(this).closest('div.project-details').toggle();
+	});
 
 
 
